6233f72a035bb227bd313fa2e38269d1a4d2da94
BalArtemiy <blackpawsstudio@gmail.com>
2021-03-13 01:43:02 +0300

    feat: solved typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == null || array.length == 0){
+    return 0;
+  }
+  else{
+    let minimal = array[0];
+    for(let i = 1; i < array.length; i++){
+      if (array[i]<minimal){
+        minimal = array[i];
+      }
+    }
+    return minimal;   
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null || array.length == 0){
+    return 0;
+  }
+  else{
+    let maximal = array[0];
+    for(let i = 1; i < array.length; i++){
+      if (array[i]>maximal){
+        maximal = array[i];
+      }
+    }
+    return maximal;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == null || array.length == 0){
+    return 0;
+  }
+  else{
+    average = 0;
+    for(let i = 0; i < array.length; i++){
+      average += array[i];
+    }
+    return average / array.length;
+  }
 }
